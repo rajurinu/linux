cb6504d756bd10f965b54217e6c707196bfef85b
RAJU RINU <rajuit2k7@gmail.com>
2024-12-28 19:41:50 +0000

    Update node_health.sh

--- node_health.sh
+++ node_health.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+################################################
+#Author: Raju Rinu
 #This script helps to detect the health of node
+#
+#Version: v1
+#################################################
+set -x #debug mode
+
+df-h
+
+free -g
+
+nproc
+
+
 
